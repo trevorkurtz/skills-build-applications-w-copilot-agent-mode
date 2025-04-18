f76c5e51e574ab70c71418e0a9d0af0c8381b9ea
trevorkurtz <trevorkurtz@outlook.com>
2025-04-18 22:43:38 +0000

    fixing endpoint reference

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = request.build_absolute_uri('/')
+    base_url = 'https://miniature-space-system-679wwv546634xv7-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
